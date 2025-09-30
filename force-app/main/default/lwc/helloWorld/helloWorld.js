e9f8c35cf3e4a0e328fb1acb6f4b207fa0b49d16
Pavankumar Kori <pavankumarkori@Pavankumars-MacBook-Air.local>
2025-09-30 12:12:24 +0530

    Initial commit

---
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -0,0 +1,37 @@
+import { LightningElement, track } from 'lwc';
+
+export default class HelloWorld extends LightningElement {
+    fullName = "Zero to Hero";
+
+    // one way data binding
+    title = "aura"
+
+    changeHandler(event){
+        this.title = event.target.value;
+    }
+
+    // two way data binding starts here
+    // using @track decorator
+
+    @track address = {
+        city: 'Bangalore',
+        state: 'Karnataka'
+    }
+
+    trackHandler(event){
+        this.address.city = event.target.value;
+    }
+
+    // without using @track decorator
+
+    country = {
+        name: '',
+        code: 'IN'
+    }
+
+    withOutTrackHandler(event){
+        this.country = {...this.country, "name" : event.target.value};
+    }
+
+    // two way data binding ends here
+}
